59b6e8acacaa7d931bbc6a1f2f17e0c651ccd9a3
Gian Lucas <lgian584@gmail.com>
2023-01-20 17:31:19 -0300

    initial config

--- src/App.jsx
+++ src/App.jsx
@@ -37,37 +37,37 @@ do formulário e zerar a barra de progresso novamente.
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <h3>desafio fernandev</h3>
       <h1>progresso do formulário</h1>
 
       <main>
         {/* crie a barra de progresso aqui */}
-        <div className='form-group'>
-          <label htmlFor=''>Nome Completo</label>
+        <div className="form-group">
+          <label htmlFor="">Nome Completo</label>
           <input />
         </div>
-        <div className='form-group'>
-          <label htmlFor=''>E-mail</label>
+        <div className="form-group">
+          <label htmlFor="">E-mail</label>
           <input />
         </div>
-        <div className='form-group'>
-          <label htmlFor=''>Estado Civil</label>
+        <div className="form-group">
+          <label htmlFor="">Estado Civil</label>
           <select>
-            <option value=''>- selecione...</option>
-            <option value='solteiro'>Solteiro</option>
-            <option value='casado'>Casado</option>
-            <option value='divorciado'>Divorciado</option>
+            <option value="">- selecione...</option>
+            <option value="solteiro">Solteiro</option>
+            <option value="casado">Casado</option>
+            <option value="divorciado">Divorciado</option>
           </select>
         </div>
-        <div className='form-group'>
-          <label htmlFor=''>Gênero</label>
-          <div className='radios-container'>
+        <div className="form-group">
+          <label htmlFor="">Gênero</label>
+          <div className="radios-container">
             <span>
-              <input type='radio' /> Masculino
+              <input type="radio" /> Masculino
             </span>
             <span>
-              <input type='radio' /> Feminino
+              <input type="radio" /> Feminino
             </span>
           </div>
         </div>
